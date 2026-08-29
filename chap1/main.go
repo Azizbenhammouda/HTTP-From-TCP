542a8940ea25c510eee1dd5639623add5538749d
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-08-29 18:05:16 +0100

    channel

--- chap1/main.go
+++ chap1/main.go
@@ -8,37 +8,50 @@ import (
 	"strings"
 )
 
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	ch := make(chan string)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		LINE := ""
+
+		for {
+			buf := make([]byte, 8)
+
+			n, err := f.Read(buf)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			parts := strings.Split(string(buf[:n]), "\n")
+
+			for i := 0; i < len(parts)-1; i++ {
+				ch <- (LINE + parts[i])
+				LINE = ""
+			}
+
+			LINE += parts[len(parts)-1]
+		}
+
+		if LINE != "" {
+			ch <- LINE
+		}
+	}()
+
+	return ch
+}
+
 func main() {
 	f, err := os.Open("messages.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
 
-	LINE := ""
-
-	for {
-		buf := make([]byte, 8)
-
-		n, err := f.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		parts := strings.Split(string(buf[:n]), "\n")
-
-		for i := 0; i < len(parts)-1; i++ {
-			fmt.Printf("read: %s%s\n", LINE, parts[i])
-			LINE = ""
-		}
-
-		LINE += parts[len(parts)-1]
-	}
-
-	if LINE != "" {
-		fmt.Printf("read: %s\n", LINE)
+	for line := range getLinesChannel(f) {
+		fmt.Printf("read: %s\n", line)
 	}
 }
